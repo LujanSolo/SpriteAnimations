79dea607692e2280a8dbf605fef179fa4a5074cc
LujanSolo <lujansolo@proton.me>
2023-01-02 01:15:48 -0800

    adjusted animate function to pull dynamically from animation states array. Still need to dynamically produce each animated state, rather than hardcode calling it

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ const playerImage = new Image();
 playerImage.src = "assets/images/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 4;
+
 let gameFrame = 0;
 const adjustFrames = 5;
 
@@ -57,23 +56,38 @@ const animationStates = [
   }
 ];
 
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  }
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+})
+console.log(spriteAnimations);
+
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  let position = Math.floor(gameFrame/adjustFrames) % 6;
-  frameX = spriteWidth * position;
-  ctx.drawImage(
-    playerImage, 
-    frameX, 
-    frameY * spriteHeight, 
-    spriteWidth,
-    spriteHeight, 
-    0, 0,
-    spriteWidth, 
-    spriteHeight
-    );
+  let position = Math.floor(gameFrame / adjustFrames) % spriteAnimations['bite'].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations['bite'].loc[position].y;
 
-    gameFrame++;
-    requestAnimationFrame(animate);
+  ctx.drawImage(
+    playerImage,
+    frameX,
+    frameY,
+    spriteWidth,
+    spriteHeight,
+    0, 0,
+    spriteWidth,
+    spriteHeight
+  );
+
+  gameFrame++;
+  requestAnimationFrame(animate);
 };
 animate();
