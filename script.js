cd25a4551230f1d121b1cb8cfd81adebc631dbba
LujanSolo <lujansolo@proton.me>
2023-01-01 22:26:25 -0800

    added animate() in reference to the animation png

--- script.js
+++ script.js
@@ -5,14 +5,30 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src='./assets/images/shadow_dog.png';
+playerImage.src="assets/images/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
-let frameY = 0;
+let frameY = 4;
 let gameFrame = 0;
 const adjustFrames = 5;
 
 function animate() {
   ctx.chearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-}
+  let position = Math.floor(gameFrame/adjustFrames) % 6;
+  frameX = spriteWidth * position;
+  ctx.drawImage(
+    playerImage, 
+    frameX, 
+    frameY * spriteHeight, 
+    spriteWidth,
+    spriteHeight, 
+    0, 0,
+    spriteWidth, 
+    spriteHeight
+    );
+
+    gameFrame++;
+    requestAnimationFrame(animate);
+};
+animate();
